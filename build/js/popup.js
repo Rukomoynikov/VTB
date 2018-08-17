6d2dfbc63b93f3a72f2bdf826278aa8a7853473a
Maksim Rukomoynikov <m.rukomoynikov@etpgpb.ru>
2018-08-17 08:39:22 +0300

    Картинки и меню

--- build/js/popup.js
+++ build/js/popup.js
@@ -1,12 +1,10 @@
+var popup = document.querySelector(".side-menu");
+var popupMenu = document.querySelector(".main-nav");
+var popupBody = document.querySelector(".cards");
 
-  var link = document.querySelector(".main-nav__toggle");
-  var popup = document.querySelector(".side-menu");
-  var popupMenu = document.querySelector(".main-nav");
-  var popupBody = document.querySelector(".cards");
-  var close = popup.querySelector(".log-out");
-
-  link.addEventListener("click", function(event) {
-        event.preventDefault();
+$(document).on("click", ".main-nav__toggle", function(event) {
+  event.preventDefault();
+  
   if (popup.classList.contains("side-menu-show")) {
     popup.classList.remove("side-menu-show");
     popupMenu.classList.remove("main-nav-show");
@@ -17,21 +15,24 @@
     popupBody.classList.add("cards-show");    
   }
 
-  });
+});
 
-    close.addEventListener("click", function(event) {
-      event.preventDefault();
+
+
+$(document).on("click", ".log-out", function(event) {
+  event.preventDefault();
+
+  popup.classList.remove("side-menu-show");
+  popupMenu.classList.remove("main-nav-show");
+  popupBody.classList.remove("cards-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("side-menu-show")) {
       popup.classList.remove("side-menu-show");
       popupMenu.classList.remove("main-nav-show");
       popupBody.classList.remove("cards-show");
-    });
-
-  window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-      if (popup.classList.contains("side-menu-show")) {
-        popup.classList.remove("side-menu-show");
-        popupMenu.classList.remove("main-nav-show");
-        popupBody.classList.remove("cards-show");
-      }
     }
-  });
+  }
+});
